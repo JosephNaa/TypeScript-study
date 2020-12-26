e5be917b3e0d8a94c1e7c90cb48bb320ec15f576
JosephNaa <js.pekah@gmail.com>
2020-12-26 18:06:13 +0900

    2020/12/26-v0.2

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,24 @@
+
+//JS
+//var str = 'hello'
+
+//TS
+let str: string = 'hello'
+let num: number = 10
+let arr: Array<number> = [1,2,3]
+let heroes: Array<string> = ['capt', 'thor', 10]
+let items: number[] = [1,2,3]
+
+let address: [string, number] = ['gangnam', 100]
+
+let obj: object = {}
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// }
+let person: {name: string, age: number} = {
+    name: 'capt',
+    age: 1000
+}
+
+let show: boolean = true
---
+++ class-note/2_functions.ts
@@ -0,0 +1,22 @@
+function sum0(a: number, b: number) {
+    return a + b
+}
+
+sum(10, 20)
+
+function add(): number {
+    return 10
+}
+
+function sum1(a: number, b:number) : number {
+    return a+b
+}
+
+sum1(10, 20, 30, 40)
+sum1(10)
+
+function log(a: string, b?: string) {
+
+}
+log('hello world')
+log('hello ts', 'abc')
